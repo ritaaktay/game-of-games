5cb335cc1c60a047e47d1983433ba79551fc30f2
Jovi <kwong.jovi@gmail.com>
2022-11-04 10:46:08 +0000

    update(index.js; cookieJar.js)

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const Level = require("./lib/level");
 const levelPlans = require("./lib/levelPlans");
 const DOMDisplay = require("./lib/DOMDisplay");
 const Game = require("./lib/game");
-const BlockJumpGame = require("./lib/blockJumpGame");
-const DumbMiniGame = require("./lib/dumbMiniGame");
 
 const level = new Level(levelPlans[0]);
 const game = new Game(level, DOMDisplay);
