7fce670cfdee9a94993eb1b4c855e1cfa4c53471
ritaaktay <ritaaktay@gmail.com>
2022-11-04 17:52:56 +0000

    test(state.test.js): mocking mini game constructor of CookieJar in State#update

--- test/unit/state.test.js
+++ test/unit/state.test.js
@@ -1,8 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const Level = require("../../lib/level");
 const State = require("../../lib/state");
 const Player = require("../../lib/player");
-const levelPlans = require("../../lib/levelPlans");
 const Vec = require("../../lib/vector");
+const levelPlans = require("../../lib/levelPlans");
+const mockLevelPlan = require("../mockLevelPlan1");
+jest.mock("../../lib/blockJumpGame");
 
 describe("State", () => {
   it("has a level, actors, status and miniGameStatus", () => {
@@ -44,5 +50,46 @@ describe("State", () => {
     const player1 = Player.create(new Vec(0, 0));
     const player2 = Player.create(new Vec(0, 0));
     expect(state.overlap(player1, player2)).toEqual(true);
+  });
+
+  it("has an update method that returns an updated state", () => {
+    const level = new Level(levelPlans[0]);
+    const state = State.start(level, level.startActors, "playing");
+    const newState = state.update(1, {});
+    expect(newState.level).toEqual(level);
+    expect(newState.actors).toEqual(level.startActors);
+    expect(newState.status).toEqual("playing");
+    expect(newState.miniGameStatus).toEqual(null);
+  });
+
+  it("has an update method that returns an updated state with movement", () => {
+    const level = new Level(levelPlans[0]);
+    const state = State.start(level, level.startActors, "playing");
+    const newState = state.update(1, {
+      "ArrowRight": true,
+    });
+    expect(newState.level).toEqual(level);
+    expect(newState.player.pos).toEqual(new Vec(6, 8));
+    expect(newState.status).toEqual("playing");
+    expect(newState.miniGameStatus).toEqual(null);
+  });
+
+  it("has an update method that returns an updated state with collision", () => {
+    const level = new Level(mockLevelPlan);
+    const mockMiniGame = {
+      run: (callback) => {},
+    };
+    const MockMiniGame = jest.fn().mockImplementation(() => {
+      return mockMiniGame;
+    });
+    level.startActors[1].miniGame = MockMiniGame;
+    const state = State.start(level, level.startActors, "playing");
+    const newState = state.update(1, {
+      "ArrowRight": true,
+    });
+    expect(newState.level).toEqual(level);
+    expect(newState.player.pos).toEqual(new Vec(6, 8));
+    expect(newState.status).toEqual("playing");
+    expect(newState.miniGameStatus).toEqual("playing");
   });
 });
