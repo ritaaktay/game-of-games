7eec26425ea46dc1d0eb7a1d17da1a2e0508661c
ritaaktay <ritaaktay@gmail.com>
2022-11-05 14:44:43 +0000

    moved DOMDisplay#trackKeys to Game#trackKeys

--- test/game.test.js
+++ test/game.test.js
@@ -1,5 +1,5 @@
 const Game = require("../lib/game");
 
-describe("", () => {
-  it("", () => {});
+describe("Game", () => {
+  it("has a level, a display, a state and an arrow keys tracker", () => {});
 });
