a622ceff1fdd41a54b270f28f7ba537844ba25f5
ritaaktay <ritaaktay@gmail.com>
2022-11-05 14:52:45 +0000

    test(game.test.js) passign test for cosntructor and dependency injections)

--- test/game.test.js
+++ test/game.test.js
@@ -1,5 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
 const Game = require("../lib/game");
+const DOMDisplay = require("../lib/DOMDisplay");
+const Level = require("../lib/level");
+const State = require("../lib/state");
+const levelPlans = require("../lib/levelPlans");
+const { default: JSDOMEnvironment } = require("jest-environment-jsdom");
+jest.mock("../lib/DOMDisplay.js");
 
 describe("Game", () => {
-  it("has a level, a display, a state and an arrow keys tracker", () => {});
+  it("has a level, a display, a state and an arrow keys tracker", () => {
+    const level = new Level(levelPlans[0]);
+    const game = new Game(level, DOMDisplay);
+    expect(game.level).toEqual(level);
+    expect(game.display instanceof DOMDisplay).toBe(true);
+    expect(game.state instanceof State).toEqual(true);
+    expect(game.state.level).toEqual(level);
+    expect(game.arrowKeysTracker).toEqual({});
+  });
 });
