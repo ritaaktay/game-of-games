000911d7ff36c4a2ae7a70cf8fdc676a49690148
ritaaktay <ritaaktay@gmail.com>
2022-11-07 09:47:53 +0000

    test(blockJumpGame.test.js): passing test for constructor

--- test/blockJumpGame.test.js
+++ test/blockJumpGame.test.js
@@ -16,5 +16,9 @@ describe("BlockJumpGame", () => {
   it("has character, a block, a jump button, a start button, and a jump counter initialized to 0", () => {
     const game = new BlockJumpGame();
     expect(game.character).toEqual(document.getElementById("character"));
+    expect(game.block).toEqual(document.getElementById("block"));
+    expect(game.jumpButton).toEqual(document.getElementById("jump-button"));
+    expect(game.startButton).toEqual(document.getElementById("start-button"));
+    expect(game.jumpCounter).toEqual(0);
   });
 });
