5f9c3a5dbdf77ae87dfb1357dc623407bfcc1e81
ritaaktay <ritaaktay@gmail.com>
2022-11-15 07:54:10 +0000

    state.js: refactoring State#update

--- bundle.js
+++ bundle.js
@@ -43,7 +43,7 @@ var require_state = __commonJS({
       update = function(time, keys, levelConstructor) {
         let actors = this.actors;
         if (this.miniGameStatus != "playing") {
-          actors = this.actors.map((actor) => actor.update(time, this, keys));
+          actors = this.updateActors(time, keys);
         }
         let newState = new State(
           this.level,
@@ -59,14 +59,17 @@ var require_state = __commonJS({
             newState = actor.collide(newState, levelConstructor);
           }
         }
+        if (this.resetMiniGameStatus(newState, player))
+          newState.miniGameStatus = null;
+        return newState;
+      };
+      resetMiniGameStatus = (state, player) => {
         const cookieJar1 = this.actors.find((actor) => actor.type == "cookieJar1");
         const cookieJar2 = this.actors.find((actor) => actor.type == "cookieJar2");
-        if (cookieJar1 != void 0 && cookieJar2 != void 0) {
-          if (!this.overlap(cookieJar1, player) && !this.overlap(cookieJar2, player) && (newState.miniGameStatus == "Won" || newState.miniGameStatus == "Lost")) {
-            newState.miniGameStatus = null;
-          }
-        }
-        return newState;
+        return cookieJar1 != void 0 && cookieJar2 != void 0 && !this.overlap(cookieJar1, player) && !this.overlap(cookieJar2, player) && (state.miniGameStatus == "Won" || state.miniGameStatus == "Lost");
+      };
+      updateActors = (time, keys) => {
+        return this.actors.map((actor) => actor.update(time, this, keys));
       };
       overlap = function(actor1, actor2) {
         return actor1.pos.x + actor1.size.x > actor2.pos.x && actor1.pos.x < actor2.pos.x + actor2.size.x && actor1.pos.y + actor1.size.y > actor2.pos.y && actor1.pos.y < actor2.pos.y + actor2.size.y;
