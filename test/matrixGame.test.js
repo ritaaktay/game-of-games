fa6bd0096244e060c07b8830cf9f869b69c1fa22
ritaaktay <ritaaktay@gmail.com>
2022-11-15 07:37:19 +0000

    matrixGame.test.js: fixed tests for dynamic HTML

--- test/matrixGame.test.js
+++ test/matrixGame.test.js
@@ -15,26 +15,22 @@ beforeEach(() => {
 });
 
 describe("MatrixGame", () => {
-  it("has a background image and an image of 2 pills", () => {
-    expect(game.image).toEqual(document.getElementById("matrix"));
-    expect(game.pills).toEqual(document.getElementById("pills"));
-  });
-
   it(".run() displays message and images", () => {
     game.run((callback) => {});
 
     expect(document.getElementById("text").textContent).toEqual(
       "Make your choice. Press [R] for the red pill, [B] for the blue pill."
     );
-    expect(game.image.style.display).toEqual("inline");
-    expect(game.pills.style.display).toEqual("inline");
+    expect(document.getElementById("matrix")).not.toBeNull();
+    expect(document.getElementById("pills")).not.toBeNull();
+    game.end();
   });
 
   it(".end() removes images", () => {
     game.run((callback) => {});
     game.end();
-    expect(game.image.style.display).toEqual("none");
-    expect(game.pills.style.display).toEqual("none");
+    expect(document.getElementById("matrix")).not.toBe();
+    expect(document.getElementById("pills")).not.toBe();
   });
 
   it("displays messages to screen", () => {
@@ -46,13 +42,10 @@ describe("MatrixGame", () => {
     const mockCallback = jest.fn().mockImplementation((result) => {
       expect(result).toEqual("Won");
       expect(spy).toHaveBeenCalled();
-      expect(game.image.style.display).toEqual("none");
-      expect(game.pills.style.display).toEqual("none");
       done();
     });
     const spy = jest.spyOn(game, "end");
     game.run(mockCallback);
-
     const event = new KeyboardEvent("keydown", { "key": "r" });
     window.dispatchEvent(event);
   });
@@ -61,13 +54,10 @@ describe("MatrixGame", () => {
     const mockCallback = jest.fn().mockImplementation((result) => {
       expect(result).toEqual("Lost");
       expect(spy).toHaveBeenCalled();
-      expect(game.image.style.display).toEqual("none");
-      expect(game.pills.style.display).toEqual("none");
       done();
     });
     const spy = jest.spyOn(game, "end");
     game.run(mockCallback);
-
     const event = new KeyboardEvent("keydown", { "key": "b" });
     window.dispatchEvent(event);
   });
