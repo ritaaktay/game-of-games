a86aa8e6204300caf8c61c08a214005209015723
ritaaktay <ritaaktay@gmail.com>
2022-11-18 10:19:59 +0000

    level.js: refactor

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Level = require("./lib/level");
 const levelPlans = require("./lib/levelPlans");
 const CanvasDisplay = require("./lib/canvasDisplay");
+const MiniGameContainer = require("./lib/miniGameContainer");
 const Game = require("./lib/game");
 
-const level = new Level(levelPlans[0]);
+const level = new Level(levelPlans[0], MiniGameContainer);
 const game = new Game(level, CanvasDisplay, Level);
 
 game.run();
+
+const level = new Level(levelPlan, new MiniGameContainer(Tetris, Pong, Snake));
