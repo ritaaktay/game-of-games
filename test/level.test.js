5e2a03e71eaa0092606ca0c78338040b3b191f50
ritaaktay <ritaaktay@gmail.com>
2022-11-25 10:43:08 +0000

    test(level.test.js): all passing

--- test/level.test.js
+++ test/level.test.js
@@ -1,26 +1,27 @@
 const Vec = require("../lib/vector");
-const mockLevelPlans = require("./mockLevelPlans");
 const Level = require("../lib/level");
+const MiniGameLocator = require("../lib/miniGameLocator");
+const mockLevelPlans = require("./mockLevelPlans");
 
-// Future: use mocks to convert this to pure unit testing
+beforeEach(() => {
+  miniGameLocator = new MiniGameLocator([{}, {}]);
+});
 
 describe("Level", () => {
   it("creates an array of arrays representing a given level plan", () => {
-    const level = new Level(mockLevelPlans.twoCookieJars);
+    const level = new Level(mockLevelPlans, miniGameLocator);
 
     expect(level.height).toEqual(9);
     expect(level.width).toEqual(18);
-    expect(level.startActors[0].type).toEqual("cookieJar2");
+    expect(level.startActors[0].type).toEqual("cookieJar");
     expect(level.startActors[1].type).toEqual("player");
-    expect(level.startActors[2].type).toEqual("cookieJar1");
+    expect(level.startActors[2].type).toEqual("cookieJar");
     expect(level.rows.length).toEqual(9);
 
-    // Assert number of elements in each child array
     for (let i = 0; i < level.rows.length; i++) {
       expect(level.rows[i].length).toEqual(18);
     }
 
-    // Assert value of each element in each child array
     for (let a = 0; a < level.rows.length; a++) {
       for (let b = 0; b < level.rows[a].length; b++) {
         expect(level.rows[a][b]).toEqual("empty");
@@ -28,26 +29,29 @@ describe("Level", () => {
     }
   });
 
-  it("touchesElement() checks if an actor is touching a given element of the game's level plan", () => {
-    const level = new Level(mockLevelPlans.twoCookieJars);
+  it("touchesWall() checks if an actor is touching a wall", () => {
+    const level = new Level(mockLevelPlans, miniGameLocator);
 
-    // An actor within the game's boundaries
     actor1 = {
       pos: new Vec(5, 5),
       size: new Vec(1, 1),
     };
 
-    // Assert the actor is not touching the level plan's boundary
-    let result1 = level.touchesElement(actor1.pos, actor1.size, "wall");
+    let result1 = level.touchesWall(actor1.pos, actor1.size, "wall");
     expect(result1).toEqual(false);
 
-    // An actor outside of the game's boundaries
     actor2 = {
       pos: new Vec(-5, -5),
       size: new Vec(1, 1),
     };
-    // Assert an actor is touching (or is inside) the level plan's boundary
-    const result2 = level.touchesElement(actor2.pos, actor2.size, "wall");
+    const result2 = level.touchesWall(actor2.pos, actor2.size, "wall");
     expect(result2).toEqual(true);
+  });
+
+  it("switches between different level plans at game stage", () => {
+    const level = new Level(mockLevelPlans, miniGameLocator);
+    const spy = jest.spyOn(level, "makeMatrix");
+    const escapeLevel = level.switch("escape");
+    expect(spy).toHaveBeenCalledWith(mockLevelPlans["escape"]);
   });
 });
