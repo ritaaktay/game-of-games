b16695a92a7cf5741f2cffe24924da8c9b04e9ca
ritaaktay <ritaaktay@gmail.com>
2022-11-25 10:47:31 +0000

    test(player.test.js): all passing

--- test/player.test.js
+++ test/player.test.js
@@ -2,16 +2,21 @@ const Player = require("../lib/player");
 const Vec = require("../lib/vector");
 const State = require("../lib/state");
 const Level = require("../lib/level");
+const MiniGameLocator = require("../lib/miniGameLocator");
 const mockLevelPlans = require("./mockLevelPlans");
+
+beforeEach(() => {
+  miniGameLocator = new MiniGameLocator([{}, {}]);
+});
 
 describe("Player", () => {
   it("has a position vector, speed vector, and a speed amount", () => {
     const player = new Player(new Vec(0, 0), new Vec(0, 0), 6);
     expect(player.pos.x).toEqual(0);
     expect(player.pos.y).toEqual(0);
-    expect(player.speed.x).toEqual(0);
-    expect(player.speed.y).toEqual(0);
-    expect(player.xySpeed).toEqual(6);
+    expect(player.velocity.x).toEqual(0);
+    expect(player.velocity.y).toEqual(0);
+    expect(player.speed).toEqual(6);
   });
 
   it("has a getter for type", () => {
@@ -23,9 +28,9 @@ describe("Player", () => {
     const player = Player.create(new Vec(0, 0));
     expect(player.pos.x).toEqual(0);
     expect(player.pos.y).toEqual(0);
-    expect(player.speed.x).toEqual(0);
-    expect(player.speed.y).toEqual(0);
-    expect(player.xySpeed).toEqual(6);
+    expect(player.velocity.x).toEqual(0);
+    expect(player.velocity.y).toEqual(0);
+    expect(player.speed).toEqual(6);
   });
 
   it("has default size", () => {
@@ -35,7 +40,7 @@ describe("Player", () => {
 
   it("it moves the player right", () => {
     const player = Player.create(new Vec(0, 0));
-    const level = new Level(mockLevelPlans.twoCookieJars);
+    const level = new Level(mockLevelPlans, miniGameLocator);
     const state = new State(level, [], "playing");
     const keys = {
       ArrowRight: true,
@@ -45,12 +50,12 @@ describe("Player", () => {
     };
     const newPlayer = player.update(2, state, keys);
     expect(newPlayer.pos).toEqual(new Vec(12, 0));
-    expect(newPlayer.speed).toEqual(new Vec(6, 0));
+    expect(newPlayer.velocity).toEqual(new Vec(6, 0));
   });
 
   it("it does not move further than level boundary on X-axis", () => {
     const player = Player.create(new Vec(6, 6));
-    const level = new Level(mockLevelPlans.twoCookieJars);
+    const level = new Level(mockLevelPlans, miniGameLocator);
     const state = new State(level, [], "playing");
     const keys = {
       ArrowRight: false,
@@ -60,12 +65,12 @@ describe("Player", () => {
     };
     const newPlayer = player.update(2, state, keys);
     expect(newPlayer.pos).toEqual(new Vec(6, 6));
-    expect(newPlayer.speed).toEqual(new Vec(-6, 0));
+    expect(newPlayer.velocity).toEqual(new Vec(-6, 0));
   });
 
   it("it does not move further than level boundary on Y-axis", () => {
     const player = Player.create(new Vec(6, 6));
-    const level = new Level(mockLevelPlans.twoCookieJars);
+    const level = new Level(mockLevelPlans, miniGameLocator);
     const state = new State(level, [], "playing");
     const keys = {
       ArrowRight: false,
@@ -75,12 +80,12 @@ describe("Player", () => {
     };
     const newPlayer = player.update(2, state, keys);
     expect(newPlayer.pos).toEqual(new Vec(6, 6));
-    expect(newPlayer.speed).toEqual(new Vec(0, 6));
+    expect(newPlayer.velocity).toEqual(new Vec(0, 6));
   });
 
   it("it moves the player up", () => {
     const player = Player.create(new Vec(12, 12));
-    const level = new Level(mockLevelPlans.twoCookieJars);
+    const level = new Level(mockLevelPlans, miniGameLocator);
     const state = new State(level, [], "playing");
     const keys = {
       ArrowRight: false,
@@ -90,12 +95,12 @@ describe("Player", () => {
     };
     const newPlayer = player.update(1, state, keys);
     expect(newPlayer.pos).toEqual(new Vec(12, 6));
-    expect(newPlayer.speed).toEqual(new Vec(0, -6));
+    expect(newPlayer.velocity).toEqual(new Vec(0, -6));
   });
 
   it("it moves the player down", () => {
     const player = Player.create(new Vec(0, 0));
-    const level = new Level(mockLevelPlans.twoCookieJars);
+    const level = new Level(mockLevelPlans, miniGameLocator);
     const state = new State(level, [], "playing");
     const keys = {
       ArrowRight: false,
@@ -105,6 +110,6 @@ describe("Player", () => {
     };
     const newPlayer = player.update(1, state, keys);
     expect(newPlayer.pos).toEqual(new Vec(0, 6));
-    expect(newPlayer.speed).toEqual(new Vec(0, 6));
+    expect(newPlayer.velocity).toEqual(new Vec(0, 6));
   });
 });
