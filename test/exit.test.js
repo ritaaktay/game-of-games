15bdfd370e4d1f50590c74cc3199c63ef3c2c4dc
ritaaktay <ritaaktay@gmail.com>
2022-11-25 10:25:00 +0000

    test(exit.test.js): all passing

--- test/exit.test.js
+++ test/exit.test.js
@@ -6,6 +6,7 @@ const Exit = require("../lib/exit");
 const Vec = require("../lib/vector");
 const State = require("../lib/state");
 const Level = require("../lib/level");
+const MiniGameLocator = require("../lib/miniGameLocator");
 const fs = require("fs");
 const path = require("path");
 const mockLevelPlans = require("./mockLevelPlans");
@@ -34,13 +35,6 @@ describe("Exit", () => {
     expect(exit.type).toEqual("exit");
   });
 
-  it("has an update method that returns a new Exit", () => {
-    const exit = Exit.create(new Vec(0, 0));
-    const newExit = exit.update(1, null, {});
-    expect(exit.pos).toEqual(new Vec(0, 0));
-    expect(exit.speed).toEqual(new Vec(0, 0));
-  });
-
   it("has a size", () => {
     const exit = Exit.create(new Vec(0, 0));
     expect(exit.size).toEqual(new Vec(1, 1));
@@ -49,11 +43,11 @@ describe("Exit", () => {
   it("has a collide method that returns a new state", () => {
     const exit = Exit.create(new Vec(0, 0));
     const state = new State(
-      new Level(mockLevelPlans.twoCookieJars),
+      new Level(mockLevelPlans, new MiniGameLocator([{}, {}])),
       [],
       "playing"
     );
-    const newState = exit.collide(state, Level);
-    expect(newState.status).toEqual("Won");
+    const newState = exit.collide(state);
+    expect(newState.status).toEqual("won");
   });
 });
