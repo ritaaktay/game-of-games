ca47d08e8f1c472e3d6844464e234375966b1891
ritaaktay <ritaaktay@gmail.com>
2022-11-25 10:20:05 +0000

    test(cookieJar.test.js): all passing

--- test/cookieMonster.test.js
+++ test/cookieMonster.test.js
@@ -6,6 +6,7 @@ const CookieMonster = require("../lib/cookieMonster");
 const Vec = require("../lib/vector");
 const State = require("../lib/state");
 const Level = require("../lib/level");
+const MiniGameLocator = require("../lib/miniGameLocator");
 const levelPlans = require("../lib/levelPlans");
 const fs = require("fs");
 const path = require("path");
@@ -15,6 +16,14 @@ beforeEach(() => {
     path.join(__dirname, "..", "index.html")
   );
   cookieMonster = new CookieMonster(new Vec(0, 0), new Vec(0, 0));
+  miniGameLocator = new MiniGameLocator([
+    () => {
+      return { getGame: () => {} };
+    },
+    () => {
+      return { getGame: () => {} };
+    },
+  ]);
 });
 
 describe("CookieMonster", () => {
@@ -37,48 +46,58 @@ describe("CookieMonster", () => {
   });
 
   it("has default size", () => {
-    // Using CookieMonster.prototype.size
-    const cookieMonster1 = CookieMonster.create(new Vec(0, 0));
-    expect(cookieMonster1.size).toEqual(new Vec(1, 1));
-  });
-
-  it("has an updated method that returns a new and identical CookieMonster", () => {
-    const level = new Level(levelPlans[0]);
-    const state = new State(level, [], "playing");
-    const newCookieMonster = cookieMonster.update();
-    expect(newCookieMonster.pos).toEqual(new Vec(0, 0));
-    expect(newCookieMonster.speed).toEqual(new Vec(0, 0));
+    expect(cookieMonster.size).toEqual(new Vec(1, 1));
   });
 
   it(".collide() displays message to user based on number of cookies obtained by player", () => {
-    const level = new Level(levelPlans[0]);
+    const level = new Level(levelPlans, miniGameLocator);
 
-    // cookieJar1Cookie = 0; cookieJar2Cookie = 0
-    const state1 = new State(level, [], "playing", null, 0, 0);
+    const state1 = new State(level, [], "playing", null);
     cookieMonster.collide(state1);
     expect(document.getElementById("text").textContent).toEqual(
       "Give me cookies!"
     );
 
-    // cookieJar1Cookie = 1; cookieJar2Cookie = 0
-    const state2 = new State(level, [], "playing", null, 1, 0);
+    const state2 = new State(
+      level,
+      [
+        { type: "cookieJar", cookies: 1 },
+        { type: "cookieJar", cookies: 0 },
+      ],
+      "playing",
+      null
+    );
     cookieMonster.collide(state2);
     expect(document.getElementById("text").textContent).toEqual(
       "Give me more cookies! You have: 🍪"
     );
 
-    // cookieJar1Cookie = 1; cookieJar2Cookie = 1
-    const state3 = new State(level, [], "playing", null, 1, 1);
+    const state3 = new State(
+      level,
+      [
+        { type: "cookieJar", cookies: 1 },
+        { type: "cookieJar", cookies: 1 },
+      ],
+      "playing",
+      null
+    );
     cookieMonster.collide(state3, Level);
     expect(document.getElementById("text").textContent).toEqual(
-      "Thanks! Now, escape before it's too late!"
+      "Mmmm delicious! Now, escape before it's too late!"
     );
 
-    // cookieJar1Cookie = 44; cookieJar2Cookie = 88
-    const state4 = new State(level, [], "playing", null, 44, 88);
+    const state4 = new State(
+      level,
+      [
+        { type: "cookieJar", cookies: 44 },
+        { type: "cookieJar", cookies: 88 },
+      ],
+      "playing",
+      null
+    );
     cookieMonster.collide(state4, Level);
     expect(document.getElementById("text").textContent).toEqual(
-      "Thanks! Now, escape before it's too late!"
+      "Mmmm delicious! Now, escape before it's too late!"
     );
   });
 });
